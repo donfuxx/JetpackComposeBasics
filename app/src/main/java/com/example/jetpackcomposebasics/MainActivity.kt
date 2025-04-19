07a7d68ba4febfe3dcb0ee73436b16fc51254acc
donfuxx <t.fuchsm@proton.me>
2025-04-19 11:57:44 +0100

    Add LazyColumn items

--- app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -60,13 +62,13 @@ fun MyAppPreview() {
 @Composable
 fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf<String>("World", "Compose")
+    names: List<String> = List(1000) { "$it" }
 ) {
-    Column(
+    LazyColumn(
         modifier = modifier.padding(vertical = 24.dp),
     ) {
-        names.forEach { name ->
-            Greeting(name)
+        items(items = names) { name ->
+            Greeting(name = name)
         }
     }
 }
