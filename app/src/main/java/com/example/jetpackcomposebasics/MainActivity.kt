9815fcdec97e1e2930c155901d74570d81bb5e27
donfuxx <t.fuchsm@proton.me>
2025-04-19 09:51:55 +0100

    Add "show more" button

--- app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
@@ -5,9 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -49,13 +50,17 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Column(
-            modifier = modifier
-                .padding(24.dp)
-                .fillMaxWidth()
-        ) {
-            Text(text = "Hello")
-            Text(text = name)
+        Row(modifier = modifier.padding(24.dp)) {
+            Column(
+                modifier = modifier
+                    .weight(1f)
+            ) {
+                Text(text = "Hello")
+                Text(text = name)
+            }
+            ElevatedButton(onClick = {}) {
+                Text(text = "Show more")
+            }
         }
     }
 }
